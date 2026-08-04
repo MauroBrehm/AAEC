921769d054cabca723c0fe7f7f83be472f9e1dc8
Mauro Brehm <maurobren@gmail.com>
2026-08-03 21:05:51 -0300

    Subo altos cambios

--- TP1/main.py
+++ TP1/main.py
@@ -1,1 +1,15 @@
-import panda 
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parent
+CSV_PATH = BASE_DIR / "modules" / "diabetes.csv"
+
+df_diabetes = pd.read_csv(CSV_PATH)
+print(df_diabetes.head())  # Muestra los datos en una tabla
+print(df_diabetes.info())  # Muestra las instancias presentes de cada atributo
+print(df_diabetes.describe())#Muestra un resumen de las estadísticas de los atributos numéricos
+
+
